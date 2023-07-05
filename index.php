330b8f0addd92ffbb36a5016919a1168b2d1f13c
giuseppelamaestra <giuseppe.la.maestra@gmail.com>
2023-07-05 18:01:14 +0200

    added axios and vue

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.4.0/axios.min.js
+"></script>
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+</head>
+<body>
+    
+
+   <script src=" ./script/script.js"></script>
+</body>
+</html>
